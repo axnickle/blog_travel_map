70dde668d1f53a33d2ee286ca67ec05be299e501
axnickle <onlychangethatmakescents@gmail.com>
2020-03-25 22:15:47 -0400

    write valication in location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,9 @@
 class Location < ActiveRecord::Base
+  #name is one of the unique columns in the locations table (see schema.rb)
+  validates :name, uniqueness: true
 
 end
 
 #Location model is building off of ActiveRecord::Base
 #Model connects to information(data) in the database to pull information route
-#then will pass information to the Controller 
+#then will pass information to the Controller
