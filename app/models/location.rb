8cd395de5643fd22388d0c766cc1df9985a657f3
axnickle <onlychangethatmakescents@gmail.com>
2020-06-01 16:34:18 -0400

    associate data model on locations and photos

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
-
+  has_many :photos
 end
 
 #Location model is building off of ActiveRecord::Base
 #Model connects to information(data) in the database to pull information route
-#then will pass information to the Controller 
+#then will pass information to the Controller
